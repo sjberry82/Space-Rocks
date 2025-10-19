de629ef8192819cfa5f3a85c539b9757c86806ad
sjberry82 <steve@secretcowlevel.com>
2025-10-19 17:26:41 -0400

    Delete space-rocks-draft directory

--- space-rocks-draft/main.gd
+++
@@ -1,7 +0,0 @@
-extends Node
-@export var rock_scene : PackedScene
-var screensize = Vector2.ZERO
-func _ready():
-	screensize = get_viewport().get_visible_rect().size
-	for i in 3:
-		spawn_rock(3)
